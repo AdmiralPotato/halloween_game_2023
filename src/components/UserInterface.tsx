fe6baa8f140561999e62e54b14bb70e57bae02fa
Admiral Potato <admiral@nuclearpixel.com>
2023-11-12 22:51:02 -0700

    Fix candy count changes destroying joystick

--- src/components/UserInterface.tsx
+++ src/components/UserInterface.tsx
@@ -1,23 +1,11 @@
 import { useGameStore } from '../store';
 import { setupUserInput } from '../modules/userInputState';
-import React, { useEffect } from 'react';
-// @ts-ignore
-import JoystickController from 'joystick-controller';
-
-type JoystickOnMove = {
-	x: number;
-	y: number;
-	leveledX: number;
-	leveledY: number;
-	angle: number;
-	distance: number;
-};
+import React from 'react';
 
 export default function UserInterface() {
 	const candies = useGameStore((state) => state.candies);
 	const timeRemaining = useGameStore((state) => state.timeRemaining);
 	const buttonStateMap = useGameStore((state) => state.buttonStateMap);
-	const joystick = useGameStore((state) => state.joystick);
 	const setButtonState = setupUserInput(buttonStateMap);
 
 	const buttonConfigs = [
@@ -25,34 +13,6 @@ export default function UserInterface() {
 		{ symbol: '🎥', name: 'camera', style: { top: 8, left: 8 } },
 		{ symbol: '🌱', name: 'seed', style: { top: 8, right: 8 } },
 	];
-
-	useEffect(() => {
-		const joystickController = new JoystickController(
-			{
-				leftToRight: true,
-				bottomToUp: true,
-				radius: 48,
-				maxRange: 48,
-				x: '64px',
-				y: '64px',
-				distortion: true,
-			},
-			(event: JoystickOnMove) => {
-				const result = { x: 0, y: 0 };
-				if (event.distance > 0) {
-					result.x = -event.x / joystickController.options.maxRange;
-					result.y = -event.y / joystickController.options.maxRange;
-				}
-				joystick.x = result.x;
-				joystick.y = result.y;
-			},
-		);
-		console.log('joystickController created', joystickController);
-		return () => {
-			console.log('joystickController destroyed');
-			joystickController.destroy();
-		};
-	});
 
 	return (
 		<div className="UserInterface">
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GameView from './components/GameView';
+import Joystick from './components/Joystick';
 import './styles.css';
 import UserInterface from './components/UserInterface';
 
@@ -8,5 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 	<div>
 		<GameView />
 		<UserInterface />
+		<Joystick />
 	</div>,
 );
